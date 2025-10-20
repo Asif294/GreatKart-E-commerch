ce403d50fbf9ccd27696a073c43836c0bf5513c2
Asif294 <asifurrahman677294@gmail.com>
2025-10-20 23:26:14 +0600

    add

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class category(models.Model):
+    category=models.CharField(max_length=100)
+    slug=models.CharField(max_length=100,unique=True)
+    description=models.TextField()
+    image=models.ImageField(upload_to='photos/categories/',blank=True)
+
+
+    class Meta:
+        verbose_name='category'
+        # verbose_plural_name='categories'
+
+
+    def __str__(self):
+        return self.category
+
