312828cff732397359791a7d740005d54873420e
Asif294 <asifurrahman677294@gmail.com>
2025-10-23 23:47:07 +0600

    require

--- users/models.py
+++ users/models.py
@@ -41,7 +41,8 @@ class Account(AbstractBaseUser):
     username = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=20)
-
+     
+    #defult true
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
     is_admin = models.BooleanField(default=False)
@@ -49,16 +50,22 @@ class Account(AbstractBaseUser):
     is_active = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
 
+
     USERNAME_FIELD = 'email'
+
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
+
     objects = MyAccountManager()
+
 
     def __str__(self):
         return self.email
 
+
     def has_perm(self, perm, obj=None):
         return self.is_admin
+    
 
     def has_module_perms(self, add_label):
         return True
