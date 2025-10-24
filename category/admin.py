f31dba977a77b5a31a6aa94310fcb853d68ab750
Asif294 <asifurrahman677294@gmail.com>
2025-10-24 12:25:30 +0600

    slag

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .import models
-admin.site.register(models.category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields ={'slug':('category',)}
+    list_display =('category','slug')
+
+admin.site.register(models.category,CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class category(models.Model):
     category=models.CharField(max_length=100)
-    slug=models.CharField(max_length=100,unique=True)
+    slug=models.SlugField(max_length=100,unique=True)
     description=models.TextField()
     image=models.ImageField(upload_to='photos/categories/',blank=True)
 
