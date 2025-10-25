fd0f176548b23a4bcf6c14e9731ae5cafea40090
Asif294 <asifurrahman677294@gmail.com>
2025-10-25 11:01:03 +0600

    store

--- category/admin.py
+++ category/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .import models
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields ={'slug':('category',)}
+    prepopulated_fields ={'slug':( 'category',)}
     list_display =('category','slug')
 
 admin.site.register(models.category,CategoryAdmin)
--- ecomarch/urls.py
+++ ecomarch/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('users.urls')),
     path('category/',include('category.urls')),
+    path('store/',include('store.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
