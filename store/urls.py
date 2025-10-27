16843cd2275f8e2c92294c9f5ce3821ed4235e2c
Asif294 <asifurrahman677294@gmail.com>
2025-10-27 22:49:54 +0600

    categoy filter

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from .import views
 urlpatterns = [
-   path("",views.store,name='store')
+   path("",views.store,name='store'),
+   path("<slug:category_slug>/", views.store, name='products_by'),
 ]
