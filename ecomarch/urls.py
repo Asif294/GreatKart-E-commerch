1a06e7d5af2949241c749bed7e0c05a0f32f2756
Asif294 <asifurrahman677294@gmail.com>
2025-10-29 00:21:19 +0600

    carts

--- ecomarch/urls.py
+++ ecomarch/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('users.urls')),
     path('category/', include('category.urls')),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
